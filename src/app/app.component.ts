95de36d6303fbe6b08a299dcd8311418f0856d66
prinsek <prinsekumar44@gmail.com>
2022-05-31 19:38:11 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+    
+  productForm: FormGroup;  
+     
+  constructor(private fb:FormBuilder) {  
+     
+    this.productForm = this.fb.group({ 
+      quantities: this.fb.array([]) ,  
+    });  
+  }  
+    
+  quantities() : FormArray {  
+    return this.productForm.get("quantities") as FormArray  
+  }  
+     
+  newQuantity(): FormGroup {  
+    return this.fb.group({  
+      qty: '',  
+      price: '',  
+    })  
+  }  
+     
+  addQuantity() {  
+    this.quantities().push(this.newQuantity());  
+  }  
+     
+  removeQuantity(i:number) {  
+    this.quantities().removeAt(i);  
+  }  
+}
